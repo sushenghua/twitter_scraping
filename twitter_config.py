efdac7aaa934e6c7e7259661680b0e16730f3f23
Shenghua Su <shenghua.su@gmail.com>
2023-12-11 21:57:02 +0800

    a tweets scraping tool

---
+++ twitter_config.py
@@ -0,0 +1,13 @@
+
+BASE_URL = 'https://twitter.com'
+HOME_URL = 'https://twitter.com/home'
+LOGIN_URL = 'https://twitter.com/login'
+
+ACCOUNTS = [
+  {
+    'username' : 'your email',
+    'password' : 'your pass',
+    'verify' :   'your verify code',
+    'proxy' : 'your proxy addr'       # e.g. socks5://127.0.0.1:9876
+  }
+]
